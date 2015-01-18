7b2ccd72c92d6a468718ad9069aaa62660b2d18f
Hakan Bilgin <hbi99@hotmail.com>
2015-01-18 16:09:31 +0100

    gruntfile and test suite

---
+++ Gruntfile.js
@@ -0,0 +1,82 @@
+'use strict';
+
+module.exports = function (grunt) {
+	grunt.initConfig({
+
+		// metadata
+		pkg : grunt.file.readJSON('package.json'),
+		meta: {
+			copyright : 'Copyright (c) 2013-<%= grunt.template.today("yyyy") %>',
+			banner    : '/* \n' +
+						' * x10.js v<%= pkg.version %> \n' +
+						' * <%= pkg.description %> \n' +
+						' * \n' +
+						' * <%= meta.copyright %>, <%= pkg.author.name %> <<%= pkg.author.email %>> \n' +
+						' * Licensed under the <%= pkg.license.type %> License \n' +
+						' */ \n',
+			source    : [
+							'src/x10.js'
+						]
+		},
+
+		// JShint version
+		jshint: {
+			files: {
+				src: '<%= meta.source %>'
+			}
+		},
+
+		// concatenation source files
+		concat: {
+			options: {
+				stripBanners: 'all',
+				banner: '<%= meta.banner %>'
+			},
+			// concat latest
+			latest: {
+				src: '<%= meta.source %>',
+				dest: 'dist/x10.js'
+			}
+		},
+
+		// uglifying concatenated file
+		uglify: {
+			options: {
+				banner: '<%= meta.banner %>',
+				mangle: true
+			},
+			// uglify latest
+			latest: {
+				src: ['<%= concat.latest.dest %>'],
+				dest: 'dist/x10.min.js'
+			}
+		},
+
+		// test tasks
+		jasmine: {
+			test: {
+				src: '<%= meta.source %>',
+				options: {
+					specs: 'tests/*.js'
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+	grunt.registerTask('default', [
+		'jshint',
+		'test',
+		'concat:latest',
+    	'uglify:latest'
+	]);
+
+	grunt.registerTask('test', [ 'jasmine' ]);
+
+};
+
---
+++ tests/template.js
@@ -0,0 +1,16 @@
+
+"use strict";
+
+describe('Test 1', function() {
+	var data = 1;
+
+	beforeEach(function() {
+		
+	});
+	
+	it('test completed.', function() {
+		expect( data ).toEqual( 1 );
+	});
+});
+
+
